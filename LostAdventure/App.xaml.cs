8154563d1a8636b29a195fddb4783f425a678ef2
soren.thomas <soren.thomas@etu.univ-smb.fr>
2025-12-08 15:13:18 +0100

    Création

---
+++ LostAdventure/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LostAdventure
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
