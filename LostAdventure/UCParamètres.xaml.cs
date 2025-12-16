43132b1395bd5e501067c8fb0e103b12f87e3cb6
Vincent Fornes <ProGamer@PROGAMER>
2025-12-16 22:48:21 +0100

    Ajout UC paramètres

---
+++ LostAdventure/UCParamètres.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LostAdventure
+{
+    /// <summary>
+    /// Logique d'interaction pour UCParamètres.xaml
+    /// </summary>
+    public partial class UCParamètres : UserControl
+    {
+        public UCParamètres()
+        {
+            InitializeComponent();
+        }
+    }
+}
